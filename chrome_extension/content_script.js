0f55292c8751bbda0fcbcd623ec287841ce9bbc4
Arseny Smirnov <arseny30@gmail.com>
2020-10-29 20:37:19 +0300

    rewrite script.js logic. kinda works but should be reviewed

--- chrome_extension/content_script.js
+++ chrome_extension/content_script.js
@@ -1,141 +1,76 @@
 function injectScriptJs() {
-    let s = document.createElement('script');
-    s.src = chrome.runtime.getURL('script.js');
-    s.onload = function () {
-        this.remove();
-    };
-    (document.head || document.documentElement).appendChild(s);
-}
-
-function experimental() {
-    window.addEventListener("message", event => {
-        // We only accept messages from ourselves
-        if (event.source != window)
-            return;
-
-        if (event.data.type && (event.data.type == "FROM_PAGE")) {
-            console.log("Content script received: " + event.data.text);
-            chrome.runtime.sendMessage({type: "FROM_PAGE", text: event.data.text}, (response) => {
-                console.log(response);
-            });
-        }
-    }, false);
+  let s = document.createElement("script");
+  s.src = chrome.runtime.getURL("script.js");
+  s.onload = function () {
+    this.remove();
+  };
+  (document.head || document.documentElement).appendChild(s);
 }
 
 let socket;
-let immunityTill = 0;
 const myID = Math.random();
 
 function initSocketIO() {
-    console.log("init socket io");
-	//socket = io('https://127.0.0.1:3000/', {transports: ['websocket']});
-	socket = io('https://52.200.6.77:3000', {transports: ['websocket']});
-    console.log('client connected')
-    socket.on('my_pong', msg => {
-        console.log(msg)
-    });
+  console.log("init socket io");
+  //socket = io('https://127.0.0.1:3000/', {transports: ['websocket']});
+  socket = io("https://52.200.6.77:3000", { transports: ["websocket"] });
+  console.log("client connected");
+  socket.on("my_pong", (msg) => {
+    console.log(msg);
+  });
 
-    socket.on('rdt.play.onclick', msg => {
-        console.log(`received play command from server on client`, msg)
-		if (msg.id === myID) {
-			console.log(`ignore command from myself`)
-			return;
-		}
-		immunityTill = Date.now() + 550;
-        document.dispatchEvent(new CustomEvent('rdt.play', {detail: msg}))
-    })
+  socket.on("rdt.play.onclick", (msg) => {
+    console.log(`received new state from server on client`, msg);
+    if (msg.id === myID) {
+      console.log(`ignore command from myself`);
+      return;
+    }
+    document.dispatchEvent(
+      new CustomEvent("rdt.receiveState", { detail: msg })
+    );
+  });
 }
 
-const storageGet =
-    keys => new Promise(resolve => chrome.storage.sync.get(keys, resolve));
-const sleep = async ms => await new Promise(r => setTimeout(r, ms));
+const storageGet = (keys) =>
+  new Promise((resolve) => chrome.storage.sync.get(keys, resolve));
+const sleep = async (ms) => await new Promise((r) => setTimeout(r, ms));
 
 const url = new URL(document.URL);
-const room_id_str = 'one_two_three_room_id';
+const room_id_str = "one_two_three_room_id";
 
 async function loadRoomId() {
-	let roomId = url.searchParams.get(room_id_str);
-	if (!roomId) {
-		roomId = (await storageGet(["roomId"])).roomId;
-		roomId = roomId + "_" + url.toString().hashCode().toString(36);
-	}
-    console.log("Got roomId=" + roomId, socket);
-    socket.emit("setRoomId", roomId);
+  let roomId = url.searchParams.get(room_id_str);
+  if (!roomId) {
+    roomId = (await storageGet(["roomId"])).roomId;
+    roomId = roomId + "_" + url.toString().hashCode().toString(36);
+  }
+  console.log("Got roomId=" + roomId, socket);
+  socket.emit("setRoomId", roomId);
 }
 
 async function doInit() {
-    if (window.hasOneTwoThree) {
-        console.log("Skip content_script.js (already initialized)");
-        return;
-    }
-    window.hasOneTwoThree = true;
+  if (window.hasOneTwoThree) {
+    console.log("Skip content_script.js (already initialized)");
+    return;
+  }
+  window.hasOneTwoThree = true;
 
-    console.log("start init OneTwoThree");
-    injectScriptJs();
-    experimental();
-    initSocketIO();
-    await loadRoomId();
+  console.log("start init OneTwoThree");
+  injectScriptJs();
+  initSocketIO();
+  await loadRoomId();
 
-    document.addEventListener('rdt.setRoomId', e => {
-        console.log(e);
-        console.log("set roomId=" + e.detail.roomId);
-        socket.emit("setRoomId", e.detail.roomId);
-    });
+  document.addEventListener("rdt.emitState", (e) => {
+    let state = e.detail;
+    state.id = myID;
+    socket.emit("rdt.play.onclick", state);
+  });
 
-    let video = null;
-	while (true) {
-		video = document.getElementsByTagName('video')[0];
-		console.log("Got", video);
-		if (video) {
-			break;
-		}
-		console.log("sleep..");
-		await sleep(500);
-		console.log("wake up..");
-	}
-	console.log("Got video DOM element");
-
-	video.addEventListener('play', e => {
-		console.log(`got event 'play' currentTime=${video.currentTime}`);
-		if (immunityTill > Date.now()) {
-			console.log("skip play event because of immunity");
-			return;
-		}
-        socket.emit('rdt.play.onclick', {id: myID, state: 'play', time: video.currentTime})
-
-	});
-	video.addEventListener('seeked', e => {
-		console.log(`got event 'seeked' currentTime=${video.currentTime}`);
-		if (immunityTill > Date.now()) {
-			console.log("skip seeked event because of immunity");
-			return;
-		} else {
-			console.log(`No immunity ${immunityTill} ${Date.now()}`);
-		}
-        socket.emit('rdt.play.onclick', {id: myID, time: video.currentTime})
-
-	});
-	video.addEventListener('pause', e => {
-		console.log(`got event 'pause' currentTime=${video.currentTime}`);
-		if (immunityTill > Date.now()) {
-			console.log("skip pause event because of immunity");
-			return;
-		}
-        socket.emit('rdt.play.onclick', {id: myID, state: 'pause', time: video.currentTime})
-	});
-
-    document.addEventListener('rdt.play.onclick', e => {
-	    e.detail.time = video.currentTime;  
-	    e.detail.id = myID;
-        socket.emit('rdt.play.onclick', e.detail)
-    });
-
-    console.log("finish init OneTwoThree");
+  console.log("finish init OneTwoThree");
 }
 
 function init() {
-    var initPromise = doInit();
+  var initPromise = doInit();
 }
-
 
 init();
--- chrome_extension/script.js
+++ chrome_extension/script.js
@@ -1,63 +1,189 @@
 function setTimeNetflix(time) {
-    const videoPlayer = netflix
-        .appContext
-        .state
-        .playerApp
-        .getAPI()
-        .videoPlayer;
+  const videoPlayer = netflix.appContext.state.playerApp.getAPI().videoPlayer;
 
-    // Getting player id
-    const playerSessionId = videoPlayer
-        .getAllPlayerSessionIds()[0]
+  // Getting player id
+  const playerSessionId = videoPlayer.getAllPlayerSessionIds()[0];
 
-    const player = videoPlayer
-        .getVideoPlayerBySessionId(playerSessionId)
-    player.seek(time * 1000);
+  const player = videoPlayer.getVideoPlayerBySessionId(playerSessionId);
+  player.seek(time * 1000);
 }
 
-const video = () => document.getElementsByTagName('video')[0];
-
-function setTime(time) {
-	if (time === video().currentTime) {
-		console.log("Ignore redundant setTime", time);
-		return;
-	}
-    console.log("Set Time to ", time);
-    if (typeof netflix !== "undefined") {
-        setTimeNetflix(time);
-    } else {
-        video().currentTime = time;
-    }
+function getVideo() {
+  return document.getElementsByTagName("video")[0];
 }
 
-document.addEventListener('rdt.play', function (e) {
-    console.log("Play Video", e.detail);
-    //document.getElementsByClassName("html5-main-video")[0].play();
-	if ('state' in e.detail) {
-        let state = e.detail.state
-        if (state === 'pause') {
-            video().pause();
-        }
-	}
+function setTime(video, time) {
+  console.log("Set Time to ", time);
+  if (typeof netflix !== "undefined") {
+    setTimeNetflix(time);
+  } else {
+    video.currentTime = time;
+  }
+}
 
-    if ('time' in e.detail) {
-        setTime(e.detail.time);
+function stateToString(state) {
+  return `paused=${state.paused} time=${state.currentTime}`;
+}
+
+class OneTwoThree {
+  // called when new State should be send to everybody in the room
+  onEmitState = undefined;
+  // called when state should be applied to the video
+  onApplyState = undefined;
+
+  // got state update from HTML events
+  updateState(state, now) {
+    state.createdAt = now;
+    this.#newState = state;
+    if (this.#newStateTimer) {
+      return;
+    }
+    this.#newStateTimer = setTimeout(() => {
+      this.#doUpdateVideoState();
+    }, 3);
+  }
+
+  // got update from server
+  receiveState(newState, now) {
+    this.#doUpdateVideoState();
+
+    // esmitate changes to ignore them later
+    let statesToIgnore = this.#genStatesToIgnore(this.#state, newState);
+    statesToIgnore.reverse();
+    this.#statesToIgnore.unshift(...statesToIgnore);
+
+    this.onApplyState(newState);
+  }
+
+  #sourceId = "TODO";
+  #state = { paused: true, position: 0 };
+  #newState = undefined;
+  #newStateTimer = undefined;
+  #statesToIgnore = [];
+
+  #similarPosition(a, b) {
+    return Math.abs(a - b) < 0.01;
+  }
+
+  #isSimilar(a, b) {
+    return (
+      a.paused === b.paused && Math.abs(a.currentTime - b.currentTime) < 200
+    );
+  }
+
+  #shouldIgnore(newState, oldState) {
+    let newStatesToIgnore = [];
+    let now = Date.now();
+    let shouldIgnore = false && this.#isSimilar(newState, oldState);
+    for (let state of this.#statesToIgnore) {
+      if (state.createdAt + 2000 < now) {
+        break;
+      }
+      if (this.#isSimilar(state, newState)) {
+        shouldIgnore = true;
+        //break;
+      }
+      newStatesToIgnore.push(state);
+    }
+    this.#statesToIgnore = newStatesToIgnore;
+    return shouldIgnore;
+  }
+
+  #genStatesToIgnore(from, to) {
+    let res = [];
+    let add = (x) => res.push(x);
+
+    from.createdAt = to.createdAt;
+    if (from.paused === to.paused) {
+      if (from.paused) {
+        add(to);
+        return res;
+      }
+      from.paused = false;
+      add(from);
+    }
+    if (!this.#similarPosition(from.currentTime, to.currentTime)) {
+      from.currentTime = to.currentTime;
+      add(from);
+    }
+    if (from.pause != to.paused) {
+      from.paused = to.paused;
+      add(from);
+    }
+    return res;
+  }
+
+  #doUpdateVideoState() {
+    if (!this.#newStateTimer) {
+      return;
+    }
+    let oldState = this.#state;
+    let newState = this.#newState;
+    console.log("state DO UPDATE", stateToString(newState));
+    this.#state = newState;
+    this.#newStateTimer = undefined;
+
+    if (this.#shouldIgnore(newState, oldState)) {
+      console.log("state IGNORE", stateToString(newState));
+      return;
     }
 
-	if ('state' in e.detail) {
-        let state = e.detail.state
-        if (state === 'play') {
-            video().play();
-        }
-	}
+    this.onEmitState?.(newState);
+  }
+}
 
-    window.postMessage({type: "FROM_PAGE", text: "Hello from the webpage!"}, "*");
+var ott = new OneTwoThree();
+
+function onStateChanged() {
+  let video = getVideo();
+  let state = { paused: video.paused, currentTime: video.currentTime };
+  console.log("state CHANGED", stateToString(state));
+
+  ott.updateState(state, Date.now());
+}
+
+function onReceiveState(state) {
+  console.log("state RECEIVE", stateToString(state));
+  ott.receiveState(state, Date.now());
+}
+
+function emitState(state) {
+  console.log("state EMIT", stateToString(state));
+  document.dispatchEvent(new CustomEvent("rdt.emitState", { detail: state }));
+}
+
+function applyState(state) {
+  console.log("state APPLY", stateToString(state));
+  let video = getVideo();
+  if (!video) {
+    return;
+  }
+  if (state.paused != video.paused && state.paused) {
+    video.pause();
+  }
+  if (Math.abs(state.currentTime - video.currentTime) > 0.01) {
+    setTime(video, state.currentTime);
+  }
+  if (state.paused != video.paused && !state.paused) {
+    video.play();
+  }
+}
+
+function initOneTwoThree() {
+  let video = getVideo();
+  if (video.onplay === onStateChanged) {
+    return;
+  }
+  video.onplay = onStateChanged;
+  video.onseeking = onStateChanged;
+  video.onpause = onStateChanged;
+
+  ott.onEmitState = emitState;
+  ott.onApplyState = applyState;
+  onStateChanged();
+}
+document.addEventListener("rdt.receiveState", (e) => {
+  onReceiveState(e.detail);
 });
-
-document.addEventListener('rdt.setTime', function (e) {
-    console.log('received', e.detail);
-    setTime(e.detail.time);
-});
-
+setInterval(initOneTwoThree, 1000);
 console.log("Script was injected");
-
